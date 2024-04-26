ecf65e422e4c9338aee02897a9ea7dd4fc6b3847
DenisHristov12 <48285397+DenisHristov12@users.noreply.github.com>
2024-04-26 15:30:35 +0300

    building the layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,37 @@
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import GlobalStyles from './styles/GlobalStyles';
+import Dashboard from './pages/Dashboard';
+import Bookings from './pages/Bookings';
+import Settings from './pages/Settings';
+import Users from './pages/Users';
+import Account from './pages/Account';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import PageNotFound from './pages/PageNotFound';
+import AppLayout from './ui/AppLayout';
+
 function App() {
-  return <div>zdr</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <BrowserRouter>
+        <Routes>
+          <Route element={<AppLayout />}>
+            <Route index element={<Navigate replace to='dashboard' />} />
+            <Route path='dashboard' element={<Dashboard />} />
+            <Route path='bookings' element={<Bookings />} />
+            <Route path='users' element={<Users />} />
+            <Route path='settings' element={<Settings />} />
+            <Route path='account' element={<Account />} />
+            <Route path='login' element={<Login />} />
+            <Route path='register' element={<Register />} />
+          </Route>
+
+          <Route path='*' element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
