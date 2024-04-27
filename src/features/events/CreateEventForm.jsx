f4ba6be14d36ade3d3db93dea546747754b89559
DenisHristov12 <48285397+DenisHristov12@users.noreply.github.com>
2024-04-28 02:43:06 +0300

    form error handling

--- src/features/events/CreateEventForm.jsx
+++ src/features/events/CreateEventForm.jsx
@@ -6,90 +6,109 @@ import Button from '../../ui/Button';
 import FileInput from '../../ui/FileInput';
 import Textarea from '../../ui/Textarea';
 import { useForm } from 'react-hook-form';
-
-const FormRow = styled.div`
-  display: grid;
-  align-items: center;
-  grid-template-columns: 24rem 1fr 1.2fr;
-  gap: 2.4rem;
-
-  padding: 1.2rem 0;
-
-  &:first-child {
-    padding-top: 0;
-  }
-
-  &:last-child {
-    padding-bottom: 0;
-  }
-
-  &:not(:last-child) {
-    border-bottom: 1px solid var(--color-grey-100);
-  }
-
-  &:has(button) {
-    display: flex;
-    justify-content: flex-end;
-    gap: 1.2rem;
-  }
-`;
-
-const Label = styled.label`
-  font-weight: 500;
-`;
-
-const Error = styled.span`
-  font-size: 1.4rem;
-  color: var(--color-red-700);
-`;
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { createEvent } from '../../services/apiEvents';
+import toast from 'react-hot-toast';
+import FormRow from '../../ui/FormRow';
 
 function CreateEventForm() {
-  const { register, handleSubmit } = useForm();
+  const { register, handleSubmit, reset, formState } = useForm();
+
+  const { errors } = formState;
+
+  const queryClient = useQueryClient();
+
+  const { isLoading: isCreating, mutate } = useMutation({
+    mutationFn: createEvent,
+    onSuccess: () => {
+      toast.success('New event successfully created!');
+
+      queryClient.invalidateQueries({ queryKey: ['events'] });
+      reset();
+    },
+    onError: (err) => toast.error(err.message),
+  });
 
   function onSubmit(data) {
-    console.log(data);
+    mutate(data);
+  }
+
+  function onError(errors) {
+    // console.log(errors);
   }
 
   return (
-    <Form onSubmit={handleSubmit(onSubmit)}>
-      <FormRow>
-        <Label htmlFor='date'>Event date</Label>
-        <Input type='date' id='date' {...register('date')} />
-      </FormRow>
-
-      <FormRow>
-        <Label htmlFor='entrance'>Entrance (lv.)</Label>
-        <Input type='number' id='entrance' {...register('entrance')} />
-      </FormRow>
-
-      <FormRow>
-        <Label htmlFor='musicType'>Music type</Label>
-        <Input type='text' id='musicType' {...register('musicType')} />
-      </FormRow>
-
-      <FormRow>
-        <Label htmlFor='alchoholType'>Alchohol type</Label>
-        <Input type='text' id='alchoholType' {...register('alchoholType')} />
-      </FormRow>
-
-      <FormRow>
-        <Label htmlFor='promotions'>Promotions</Label>
-        <Input type='text' id='promotions' {...register('promotions')} />
-      </FormRow>
-
-      <FormRow>
-        <Label htmlFor='description'>Description</Label>
-        <Textarea
-          type='text'
-          id='description'
-          defaultValue=''
-          {...register('description')}
+    <Form onSubmit={handleSubmit(onSubmit, onError)}>
+      <FormRow label='Event date' error={errors?.date?.message}>
+        <Input
+          type='date'
+          id='date'
+          disabled={isCreating}
+          {...register('date', {
+            required: 'This field is required',
+          })}
         />
       </FormRow>
 
-      <FormRow>
-        <Label htmlFor='image'>Event poster</Label>
-        <FileInput id='image' accept='image/*' />
+      <FormRow label='Entrance' error={errors?.entrance?.message}>
+        <Input
+          type='number'
+          id='entrance'
+          disabled={isCreating}
+          {...register('entrance', {
+            required: 'This field is required',
+            validate: (value) => value >= 0 || 'Entrance must be 0 or greater!',
+          })}
+        />
+      </FormRow>
+
+      <FormRow label='Music tyoe' error={errors?.musicType?.message}>
+        <Input
+          type='text'
+          id='musicType'
+          disabled={isCreating}
+          {...register('musicType', {
+            required: 'This field is required',
+          })}
+        />
+      </FormRow>
+
+      <FormRow label='Alchohol type' error={errors?.alchoholType?.message}>
+        <Input
+          type='text'
+          id='alchoholType'
+          disabled={isCreating}
+          {...register('alchoholType', {
+            required: 'This field is required',
+          })}
+        />
+      </FormRow>
+
+      <FormRow label='Promotions' error={errors?.promotions?.message}>
+        <Input
+          type='text'
+          id='promotions'
+          disabled={isCreating}
+          {...register('promotions', {
+            required: 'This field is required',
+          })}
+        />
+      </FormRow>
+
+      <FormRow label='Description' error={errors?.description?.message}>
+        <Textarea
+          type='text'
+          id='description'
+          disabled={isCreating}
+          defaultValue=''
+          {...register('description', {
+            required: 'This field is required',
+          })}
+        />
+      </FormRow>
+
+      <FormRow label='Event poster'>
+        <FileInput id='image' disabled={isCreating} accept='image/*' />
       </FormRow>
 
       <FormRow>
@@ -97,7 +116,7 @@ function CreateEventForm() {
         <Button variation='secondary' type='reset'>
           Cancel
         </Button>
-        <Button>Add event</Button>
+        <Button disabled={isCreating}>Add event</Button>
       </FormRow>
     </Form>
   );
--- src/services/apiEvents.js
+++ src/services/apiEvents.js
@@ -11,6 +11,17 @@ export async function getEvents() {
   return data;
 }
 
+export async function createEvent(newEvent) {
+  const { data, error } = await supabase.from('events').insert([newEvent]);
+
+  if (error) {
+    console.error(error);
+    throw new Error('Events could not be created');
+  }
+
+  return data;
+}
+
 export async function deleteEvent(id) {
   const { data, error } = await supabase.from('events').delete().eq('id', id);
 
