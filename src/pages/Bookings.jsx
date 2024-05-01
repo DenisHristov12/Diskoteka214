3e826e2be17542991ef23d49c24c9f595da10798
DenisHristov12 <48285397+DenisHristov12@users.noreply.github.com>
2024-05-01 17:32:23 +0300

    import

--- src/pages/Bookings.jsx
+++ src/pages/Bookings.jsx
@@ -1,5 +1,17 @@
+import BookingTable from '../features/bookings/BookingTable';
+import Heading from '../ui/Heading';
+import Row from '../ui/Row';
+
 function Bookings() {
-  return <div>Bookings</div>;
+  return (
+    <>
+      <Row type='horizontal'>
+        <Heading as='h1'>All bookings</Heading>
+      </Row>
+
+      <BookingTable />
+    </>
+  );
 }
 
 export default Bookings;
