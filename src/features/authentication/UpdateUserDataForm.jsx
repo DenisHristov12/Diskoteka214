049ffee0f270f0e680da8dfb143e50a346b4430f
DenisHristov12 <48285397+DenisHristov12@users.noreply.github.com>
2024-05-04 03:18:33 +0300

    update user data (have to fix avatar error)

--- src/features/authentication/UpdateUserDataForm.jsx
+++ src/features/authentication/UpdateUserDataForm.jsx
@@ -1,16 +1,13 @@
-import { useState } from "react";
-
-import Button from "../../ui/Button";
-import FileInput from "../../ui/FileInput";
-import Form from "../../ui/Form";
-import FormRow from "../../ui/FormRow";
-import Input from "../../ui/Input";
-
-import { useUser } from "./useUser";
-import { useUpdateUser } from "./useUpdateUser";
+import { useUser } from '../authentication/useUser';
+import { useState } from 'react';
+import Button from '../../ui/Button';
+import FileInput from '../../ui/FileInput';
+import Form from '../../ui/Form';
+import FormRow from '../../ui/FormRow';
+import Input from '../../ui/Input';
+import { useUpdateUser } from './useUpdateUser';
 
 function UpdateUserDataForm() {
-  // We don't need the loading state, and can immediately use the user data, because we know that it has already been loaded at this point
   const {
     user: {
       email,
@@ -18,14 +15,18 @@ function UpdateUserDataForm() {
     },
   } = useUser();
 
-  const { updateUser, isUpdating } = useUpdateUser();
-
   const [fullName, setFullName] = useState(currentFullName);
   const [avatar, setAvatar] = useState(null);
 
+  const { updateUser, isUpdating } = useUpdateUser();
+
   function handleSubmit(e) {
     e.preventDefault();
-    if (!fullName) return;
+
+    if (!fullName) {
+      return;
+    }
+
     updateUser(
       { fullName, avatar },
       {
@@ -37,43 +38,35 @@ function UpdateUserDataForm() {
     );
   }
 
-  function handleCancel() {
+  function handleCancel(e) {
     setFullName(currentFullName);
     setAvatar(null);
   }
 
   return (
     <Form onSubmit={handleSubmit}>
-      <FormRow label="Email address">
+      <FormRow label='Email address'>
         <Input value={email} disabled />
       </FormRow>
-
-      <FormRow label="Full name">
+      <FormRow label='Full name'>
         <Input
-          type="text"
+          type='text'
           value={fullName}
           onChange={(e) => setFullName(e.target.value)}
-          id="fullName"
           disabled={isUpdating}
+          id='fullName'
         />
       </FormRow>
-
-      <FormRow label="Avatar image">
+      <FormRow label='Avatar image'>
         <FileInput
-          id="avatar"
-          accept="image/*"
-          onChange={(e) => setAvatar(e.target.files[0])}
           disabled={isUpdating}
+          id='avatar'
+          accept='image/*'
+          onChange={(e) => setAvatar(e.target.files[0])}
         />
       </FormRow>
-
       <FormRow>
-        <Button
-          type="reset"
-          variation="secondary"
-          disabled={isUpdating}
-          onClick={handleCancel}
-        >
+        <Button onClick={handleCancel} type='reset' variation='secondary'>
           Cancel
         </Button>
         <Button disabled={isUpdating}>Update account</Button>
--- src/features/authentication/UserAvatar.jsx
+++ src/features/authentication/UserAvatar.jsx
@@ -23,15 +23,16 @@ const Avatar = styled.img`
 
 function UserAvatar() {
   const { user } = useUser();
-  const { fullName, avatar } = user?.user_metadata;
+  console.log(user);
+  // const { fullName, avatar } = user.user_metadata;
 
   return (
     <StyledUserAvatar>
       <Avatar
-        src={avatar || 'default-user.jpg'}
-        alt={`Avatar of ${fullName}`}
+      // src={avatar || 'default-user.jpg'}
+      // alt={`Avatar of ${fullName}`}
       />
-      <span>{fullName}</span>
+      {/* <span>{fullName}</span> */}
     </StyledUserAvatar>
   );
 }
