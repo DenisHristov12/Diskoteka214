74a7958b9c0b9a2a5c8a88f1073eb5361b4d61d4
DenisHristov12 <48285397+DenisHristov12@users.noreply.github.com>
2024-05-17 23:53:45 +0300

    set up user filters and sorts

--- src/features/users/UserTableOperations.jsx
+++ src/features/users/UserTableOperations.jsx
@@ -1,5 +1,41 @@
+import Filter from '../../ui/Filter';
+import SortBy from '../../ui/SortBy';
+import TableOperations from '../../ui/TableOperations';
+
 function UserTableOperations() {
-  return <div></div>;
+  return (
+    <TableOperations>
+      <Filter
+        filterField='role'
+        options={[
+          { value: 'all', label: 'All' },
+          { value: 'admin', label: 'Admins' },
+          { value: 'user', label: 'Users' },
+        ]}
+      />
+
+      <SortBy
+        options={[
+          {
+            value: 'created-desc',
+            label: 'Sort by created date (recent first)',
+          },
+          {
+            value: 'created-asc',
+            label: 'Sort by created date (earlier first)',
+          },
+          {
+            value: 'name-desc',
+            label: 'Sort by name (Z-A)',
+          },
+          {
+            value: 'name-asc',
+            label: 'Sort by name (A-Z)',
+          },
+        ]}
+      />
+    </TableOperations>
+  );
 }
 
 export default UserTableOperations;
