52eda45a179fe879b434457fcae5276800e96acb
DenisHristov12 <48285397+DenisHristov12@users.noreply.github.com>
2024-06-19 15:16:58 +0300

    organize code

---
+++ src/ui/EventDetail.jsx
@@ -0,0 +1,324 @@
+import styled from 'styled-components';
+import { useNavigate } from 'react-router-dom';
+import { useUser } from '../features/authentication/useUser';
+import { useEvent } from '../features/events/useEvent';
+import { useMoveBack } from '../hooks/useMoveBack';
+import useWidth from '../hooks/useWidth';
+import Spinner from './Spinner';
+import Empty from './Empty';
+import {
+  respondToLandscapeTablets,
+  respondToMobile,
+  respondToMobileSmall,
+} from '../styles/mediaQueries';
+import { format, isToday } from 'date-fns';
+import { formatDistanceFromNow } from '../utils/helpers';
+import ButtonGroup from './ButtonGroup';
+import Modal from './Modal';
+import Button from './Button';
+import ReserveForm from '../features/events/ReserveForm';
+import CreateEventForm from '../features/events/CreateEventForm';
+import ConfirmDelete from './ConfirmDelete';
+import { useDeleteEvent } from '../features/events/useDeleteEvent';
+import { useReservators } from '../features/events/useReservators';
+
+const Container = styled.div`
+  display: grid;
+  grid-template-columns: 2fr 1fr;
+  column-gap: 12.8rem;
+
+  ${respondToLandscapeTablets(`
+    gap: 2.4rem;
+
+    grid-template-columns: 2fr 1.5fr;
+  `)}
+
+  ${respondToMobile(`
+    grid-template-columns: 1fr;
+
+    justify-content: center;
+  `)}
+`;
+
+const DataBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 2.4rem;
+
+  ${respondToLandscapeTablets(`
+    gap: 1.6rem;
+  `)}
+
+  ${respondToMobile(`
+    gap: 1.4rem;
+  `)}
+`;
+
+const Header = styled.header`
+  background-color: var(--color-brand-500);
+  padding: 2rem 4rem;
+  color: #e0e7ff;
+  font-size: 1.8rem;
+  font-weight: 500;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  ${respondToLandscapeTablets(`
+    padding: 1.6rem 3.6rem;
+  `)}
+
+  ${respondToMobile(`
+    padding: 1.2rem 3rem;
+
+    font-size: 1.4rem;
+  `)}
+
+  ${respondToMobileSmall(`
+    padding: 1rem 1.4rem;
+
+    font-size: 1rem;
+  `)}
+`;
+
+const Image = styled.img`
+  height: auto;
+  width: 100%;
+
+  justify-self: center;
+  align-self: center;
+
+  ${respondToMobile(`width: 90%;`)}
+`;
+
+const Section = styled.section`
+  padding: 3.2rem 4rem;
+
+  font-size: 1.8rem;
+
+  display: flex;
+  flex-direction: column;
+  gap: 2.4rem;
+
+  ${respondToLandscapeTablets(`
+    padding: 2.4rem 3rem;
+
+    gap: 2rem;
+
+     font-size: 1.6rem;
+  `)}
+
+  ${respondToMobile(`
+    gap: 1.4rem;
+
+    font-size: 1.4rem;
+
+    padding: 2rem 2.4rem;
+  `)}
+
+  ${respondToMobileSmall(`
+    gap: 1.2rem;
+
+    font-size: 1.2rem;
+
+    padding: 1.4rem 2rem;
+  `)}
+`;
+
+const SectionDiv = styled.div`
+  height: 6rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  padding: 2.4rem;
+
+  background-color: var(--color-green-100);
+
+  & p {
+    font-weight: 600;
+  }
+
+  ${respondToLandscapeTablets(`
+     padding: 2rem;
+  `)}
+
+  ${respondToMobile(`
+     padding: 1.6rem;
+  `)}
+
+  ${respondToMobileSmall(`
+     padding: 1.2rem;
+
+     height: 4.5rem;
+  `)}
+`;
+
+const Footer = styled.footer`
+  padding: 1.6rem 4rem;
+  font-size: 1.2rem;
+  color: var(--color-grey-500);
+  text-align: right;
+
+  display: flex;
+  justify-content: center;
+
+  ${respondToMobileSmall(`
+    padding: 1rem 2.4rem;
+  font-size: 1rem;
+  `)}
+`;
+
+function EventDetail() {
+  const { event, isLoading } = useEvent();
+  const { deleteEvent, isLoading: isDeleting } = useDeleteEvent();
+  const { reservators, isLoading: isLoadingReservators } = useReservators();
+
+  const { user, isAdmin, isUser } = useUser();
+  const moveBack = useMoveBack();
+  const navigate = useNavigate();
+
+  const width = useWidth();
+
+  if (isLoading || isLoadingReservators) {
+    return <Spinner />;
+  }
+
+  if (!event) {
+    return <Empty resource='event' />;
+  }
+
+  const {
+    id: eventId,
+    created_at,
+    date,
+    name,
+    entrance,
+    musicType,
+    alchoholType,
+    promotions,
+    description,
+    image,
+    capacity,
+  } = event;
+
+  const isReservator =
+    reservators.length !== 0
+      ? reservators.some(
+          (res) =>
+            res?.fullName === user?.fullName &&
+            res?.number === user?.number &&
+            res?.eventId === eventId
+        )
+      : false;
+
+  return (
+    <>
+      <Header>
+        <p>{name}</p>
+        <p>
+          {format(new Date(date), 'EEE, MMM dd yyyy')} (
+          {isToday(new Date(date)) ? 'Today' : formatDistanceFromNow(date)})
+        </p>
+      </Header>
+      <Container>
+        {width < 430 && <Image src={image} alt='event poster' />}
+        <DataBox>
+          <Section>
+            <SectionDiv>
+              <p>Music</p>
+              <span>{musicType}</span>
+            </SectionDiv>
+            <SectionDiv>
+              <p>Alchohol</p>
+              <span>{alchoholType}</span>
+            </SectionDiv>
+            <SectionDiv>
+              <p>Promotions</p>
+              <span>{promotions !== '' ? promotions : 'None'}</span>
+            </SectionDiv>
+            {description && (
+              <SectionDiv>
+                <p>Description</p>
+                <span>{description}</span>
+              </SectionDiv>
+            )}
+            <SectionDiv>
+              <p>Entrance</p>
+              <span>{`${entrance !== 0 ? entrance + ' lv.' : 'free'}`}</span>
+            </SectionDiv>
+
+            <SectionDiv>
+              <p>Places left</p>
+              <span>{capacity}</span>
+            </SectionDiv>
+          </Section>
+
+          <ButtonGroup>
+            <Modal>
+              {isUser && !isReservator ? (
+                <>
+                  <Modal.Open opens='reserve'>
+                    <Button>Reserve</Button>
+                  </Modal.Open>
+                  <Modal.Window name='reserve'>
+                    <ReserveForm />
+                  </Modal.Window>
+                </>
+              ) : isUser ? (
+                <Button disabled='true' variation='secondary'>
+                  Reserved
+                </Button>
+              ) : (
+                <Button onClick={() => navigate('/login')}>Reserve</Button>
+              )}
+
+              {isAdmin && (
+                <>
+                  <Modal.Open opens='edit'>
+                    <Button>Edit</Button>
+                  </Modal.Open>
+                  <Modal.Window name='edit'>
+                    <CreateEventForm eventToEdit={event} />
+                  </Modal.Window>
+
+                  <Modal.Open opens='delete'>
+                    <Button variation='danger'>Delete</Button>
+                  </Modal.Open>
+
+                  <Modal.Window name='delete'>
+                    <ConfirmDelete
+                      resourceName='booking'
+                      disabled={isDeleting}
+                      onConfirm={() =>
+                        deleteEvent(eventId, {
+                          onSettled: () => navigate(-1),
+                        })
+                      }
+                    />
+                  </Modal.Window>
+                </>
+              )}
+            </Modal>
+
+            <Button variation='secondary' onClick={moveBack}>
+              Back
+            </Button>
+          </ButtonGroup>
+
+          {isAdmin && (
+            <Footer>
+              <p>
+                Created {format(new Date(created_at), 'EEE, MMM dd yyyy, p')}
+              </p>
+            </Footer>
+          )}
+        </DataBox>
+
+        {width > 430 && <Image src={image} alt='event poster' />}
+      </Container>
+    </>
+  );
+}
+
+export default EventDetail;
